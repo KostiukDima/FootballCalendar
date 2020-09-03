3160dbcdb9e178dcf498edb0145ef00d988b5304
Dima Kostiuk <DimaKostiuk@outlook.com>
2020-09-03 09:57:11 +0300

    Create

---
+++ FootballСalendar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FootballСalendar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
